22babbda959c7a0f87e988a083b35d99a0341e66
guneyizol <guneyizol@gmail.com>
2022-12-06 22:36:47 +0300

    delete semicolons

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<h1>Hello, world!</h1>)
