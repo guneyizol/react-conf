7ae60c3de34103146856570f1ce9cd2d07ba5dad
guneyizol <guneyizol@gmail.com>
2022-12-06 22:34:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+installs and configures a react development environment with the latest dependencies
+
+- run `npm i` to install dependencies
+- run `npm start` to build and run the project
+- go to localhost:3000 on your browser
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import HtmlWebpackPlugin from "html-webpack-plugin"
+const path = await import('node:path')
+
+export default {
+    mode: 'development',
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-react',
+                                {
+                                    runtime: 'automatic'
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+    devServer: {
+        port: 3000
+    }
+}
